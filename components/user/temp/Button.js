5059947e79a415946949e860759a81d04b55188b
omar <omaralaa619@gmail.com>
2026-02-26 16:18:35 +0200

    temp

--- components/user/temp/Button.js
+++ components/user/temp/Button.js
@@ -4,7 +4,7 @@ const Button = ({ text, onclick, className }) => {
   return (
     <button
       onClick={onclick}
-      className={` text-center rounded-full w-[100%] md:px-20 py-3  hover:opacity-70 hover:text-black transition-opacity duration-300 md:w-auto ${className}`}
+      className={`text-center rounded-full w-[100%] md:px-20 py-3  hover:opacity-70 hover:text-black transition-opacity duration-300 md:w-auto ${className}`}
     >
       {text}
     </button>
